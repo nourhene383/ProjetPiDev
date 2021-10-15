b1e4889e56682e367fb8a7e6502eb00540ea7abe
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2021-10-15 02:41:53 +0100

    last update

---
+++ ProjetPI/src/sample/controles/test.java
@@ -0,0 +1,110 @@
+package sample.controles;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class test {
+    @FXML
+    private TextField textlastN;
+
+    @FXML
+    private TextField textfirstN;
+
+    @FXML
+    private TextField fieldnbr;
+
+    @FXML
+    private TextField fieldcin;
+
+    @FXML
+    private DatePicker fielddate;
+
+    @FXML
+    private Button insertButton;
+
+    @FXML
+    private Label connlabel;
+
+    @FXML
+    private TableView<?> TableView;
+
+    @FXML
+    private TableColumn<?, ?> idColumn;
+
+    @FXML
+    private TableColumn<?, ?> lastColumn;
+
+    @FXML
+    private TableColumn<?, ?> firstColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneColumn;
+
+    @FXML
+    private TableColumn<?, ?> cinColumn;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextField filterfield;
+
+    @FXML
+    private ImageView QRcode;
+
+    @FXML
+    private ComboBox<?> comboP;
+
+    @FXML
+    private Label Location;
+
+    @FXML
+    void btnFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboboxx(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMouseClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleSaveAs(ActionEvent event) {
+
+    }
+
+    @FXML
+    void insertButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateButton(ActionEvent event) {
+
+    }
+}
